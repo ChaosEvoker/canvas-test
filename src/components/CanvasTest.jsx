f8d458cbf44a5cd3a2d3c9049894edf95251827b
Phillip Sipe <phillip@tailwindapp.com>
2020-04-10 11:23:58 -0500

    switching to url image

--- src/components/CanvasTest.jsx
+++ src/components/CanvasTest.jsx
@@ -8,16 +8,14 @@ const HEIGHT = 133.5;
 
 const BigImage = () => {
   const [bigImg] = useImage(bigImage);
-  const [imgURL] = useImage('https://static.showit.co/1200/Yas2uqNkTBSLjiC2ZMD-PA/66410/three16_photography-678.jpg');
-  console.log(bigImg, imgURL);
-  return <Image width={WIDTH} height={HEIGHT} image={imgURL} />;
+  return <Image width={WIDTH} height={HEIGHT} image={bigImg} />;
 }
 
 const BigImageCanvas = (props) => {
   return (
     <Stage className="big-img-canvas" width={WIDTH} height={HEIGHT}>
       <Layer>
-        <BigImage />
+        <URLImage src={bigImage} x={0} y={0} />
       </Layer>
       <Layer>
         <Rect x={20} y={13} width={160} height={107.5} fill={props.color} />
@@ -29,12 +27,59 @@ const BigImageCanvas = (props) => {
   );
 }
 
+class URLImage extends React.Component {
+  state = {
+    image: null
+  };
+  componentDidMount() {
+    this.loadImage();
+  }
+  componentDidUpdate(oldProps) {
+    if (oldProps.src !== this.props.src) {
+      this.loadImage();
+    }
+  }
+  componentWillUnmount() {
+    this.image.removeEventListener('load', this.handleLoad);
+  }
+  loadImage() {
+    // save to "this" to remove "load" handler on unmount
+    this.image = new window.Image();
+    this.image.src = this.props.src;
+    this.image.addEventListener('load', this.handleLoad);
+  }
+  handleLoad = () => {
+    // after setState react-konva will update canvas and redraw the layer
+    // because "image" property is changed
+    this.setState({
+      image: this.image
+    });
+    // if you keep same image object during source updates
+    // you will have to update layer manually:
+    // this.imageNode.getLayer().batchDraw();
+  };
+  render() {
+    console.log('boo')
+    return (
+      <Image
+        x={this.props.x}
+        y={this.props.y}
+        width={WIDTH}
+        height={HEIGHT}
+        image={this.state.image}
+        ref={node => {
+          this.imageNode = node;
+        }}
+      />
+    );
+  }
+}
+
 class CanvasTest extends React.Component {
 
   render() {
     return (
       <div className="canvas-test">
-        <img src={bigImage} />
         {Array(this.props.testSize).fill(<BigImageCanvas text={this.props.text} color={this.props.color} />)}
       </div>
     )
